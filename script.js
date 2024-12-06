766dcd4f7671613971e31ffb1eb62d7f2bd17e92
zhikko0 <sebastian.valdes@allmankurs.se>
2024-12-06 22:11:34 +0100

    Lite syle

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 import {questions} from './array.js';
 console.log(questions)
+
+let playerName = document.getElementById('player-name');
+const startGameButton = document.getElementById('start-button');
+//let playerScore = 0;
+
+startGameButton.addEventListener('click', () => {
+    let playerNameDisplay = playerName.value.trim() || 'Anonymous';
+    console.log(playerNameDisplay)
+    playerName.value = "";
+
+})
