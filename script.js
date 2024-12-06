cc6021b3dc9ed1c3c69be9bb1c3a02865999f8c4
zhikko0 <sebastian.valdes@allmankurs.se>
2024-12-06 23:24:14 +0100

    Change color container

--- script.js
+++ script.js
@@ -8,13 +8,24 @@ const startGameButton = document.getElementById('start-button');
 let changeBackgroundColor = document.getElementById('change-color');
 changeBackgroundColor.addEventListener('click', () => {
     
-    const currentBackgroundColor = document.body.style.backgroundColor;
-    if (currentBackgroundColor === 'rgb(48, 22, 74)') {
+    const currentBodyBackgroundColor = document.body.style.backgroundColor;
+    if (currentBodyBackgroundColor === 'rgb(48, 22, 74)') {
         document.body.style.backgroundColor = '#692A91';
     } else {
         document.body.style.backgroundColor = '#30164A';
     }
+
+    const currentContainerBackground = document.querySelector('.container').style.backgroundColor;
+    if (currentContainerBackground === 'rgb(124, 124, 124)') {
+        document.querySelector('.container').style.backgroundColor = 'rgb(255, 255, 255)'; 
+    } else {
+        document.querySelector('.container').style.backgroundColor = 'rgb(124, 124, 124)';
+    }
+    
+
 });
+
+
 
 
 
