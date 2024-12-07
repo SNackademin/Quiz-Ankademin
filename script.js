b153b8b59393dc511d681acb39d0c6d0836a7deb
zhikko0 <sebastian.valdes@allmankurs.se>
2024-12-07 03:41:18 +0100

    Change color input

--- script.js
+++ script.js
@@ -5,23 +5,38 @@ let playerName = document.getElementById('player-name');
 const startGameButton = document.getElementById('start-button');
 //let playerScore = 0;
 
-let changeBackgroundColor = document.getElementById('change-color');
-changeBackgroundColor.addEventListener('click', () => {
-    
+let changeColor = document.getElementById('change-color');
+changeColor.addEventListener('click', () => {
+    //change body color
     const currentBodyBackgroundColor = document.body.style.backgroundColor;
     if (currentBodyBackgroundColor === 'rgb(48, 22, 74)') {
         document.body.style.backgroundColor = '#692A91';
     } else {
         document.body.style.backgroundColor = '#30164A';
     }
-
+    //change container color
     const currentContainerBackground = document.querySelector('.container').style.backgroundColor;
     if (currentContainerBackground === 'rgb(124, 124, 124)') {
         document.querySelector('.container').style.backgroundColor = 'rgb(255, 255, 255)'; 
     } else {
         document.querySelector('.container').style.backgroundColor = 'rgb(124, 124, 124)';
     }
-    
+    //change input color
+    const currentInputBackground = document.getElementById('player-name').style.backgroundColor;;
+    if (currentInputBackground === 'rgb(132, 131, 131)') {
+        document.getElementById('player-name').style.backgroundColor = 'rgb(255, 255, 255)'; 
+    } else {
+        document.getElementById('player-name').style.backgroundColor = 'rgb(132, 131, 131)';
+    }
+    //change text input color // doesnt work
+    // const currentInputTextColor = document.getElementById('player-name');
+    // if (currentInputTextColor.style.color === 'rgb(132, 131, 131)') {
+    //     currentInputTextColor.style.color = 'rgb(255, 255, 255)' ;
+    // } else {
+    //     currentInputTextColor.style.color = 'rgb(132, 131, 131)';
+    // }
+
+
 
 });
 
